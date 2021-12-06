77d0356e3651ee886643302c9970080600ca00d8
Cot003 <barunsmusic7521@gmail.com>
2021-12-06 15:56:58 +0530

    Completed video 36

--- src/components/News.js
+++ src/components/News.js
@@ -34,16 +34,20 @@ export class News extends Component {
     }
 
     async updateNews() {
+        this.props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2c7e73a38f064c34a55b46061e6f20c6&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json()
+        this.props.setProgress(70); 
         console.log(parsedData)
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false,
         })
+        this.props.setProgress(100);
     }
 
     async componentDidMount() {
